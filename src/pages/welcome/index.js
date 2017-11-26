7f6ff857dee982a5fe701ffa9c06707d132b0950
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-11-26 19:43:29 -0300

    Add function to welcome button

--- src/pages/welcome/index.js
+++ src/pages/welcome/index.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import { NavigationActions } from 'react-navigation';
+import PropTypes from 'prop-types';
 
 import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 export default class Welcome extends Component {
+  static propTypes = {
+    navigation: PropTypes.shape({
+      dispatch: PropTypes.func,
+    }).isRequired,
+  }
 
   static navigationOptions = {
     header: null,
   }
 
-  render(){
+  navigateToUser = () => {
+    const { dispatch } = this.props.navigation;
+
+    const resetAction = NavigationActions.reset({
+      index: 0,
+      actions: [
+        NavigationActions.navigate({ routeName: 'User' }),
+      ],
+    });
+
+    dispatch(resetAction);
+  };
+
+  render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcomeTitle}>Bem-vindo</Text>
@@ -23,7 +43,7 @@ export default class Welcome extends Component {
           placeholder="Digite seu usuário"
         />
 
-        <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <TouchableOpacity style={styles.button} onPress={this.navigateToUser}>
           <Text style={styles.buttonText}>Prosseguir</Text>
         </TouchableOpacity>
       </View>
