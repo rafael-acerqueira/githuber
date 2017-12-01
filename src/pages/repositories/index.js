66fb39dae6bdd897b3fcdbfd370c67cfe43c3ade
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-12-01 14:15:53 -0300

    List repositories come from API in repositories page through Repository component

--- src/pages/repositories/index.js
+++ src/pages/repositories/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-
-import { View } from 'react-native';
+import { View, Text, AsyncStorage, ActivityIndicator } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import api from '../../services/api';
 import styles from './styles';
 import Repository from './components/Repository';
 
@@ -12,16 +12,41 @@ export default class Repositories extends Component {
     ),
   };
 
+  state = {
+    repositories: [],
+    loading: false,
+  };
+
+  componentDidMount() {
+    this.loadRepositories();
+  }
+
+  loadRepositories = async () => {
+    this.setState({ loading: true });
+    const username = await AsyncStorage.getItem('@Githuber:username');
+    const response = await api.get(`users/${username}/repos`);
+    this.setState({ repositories: response.data, loading: false });
+  };
+
+  renderRepositories = () => (
+    this.state.repositories.map(repository => (
+      <Repository key={repository.id} repository={repository} />
+    ))
+  );
+
+  renderList = () => (
+    this.state.repositories.length
+      ? this.renderRepositories()
+      : <Text style={styles.empty}>Nenhum repositório encontrado</Text>
+  );
+
   render() {
     return (
       <View style={styles.container}>
-        <Repository />
-        <Repository />
-        <Repository />
-        <Repository />
-        <Repository />
-        <Repository />
-        <Repository />
+
+        { this.state.loading
+          ? <ActivityIndicator size="small" color="#999" />
+          : this.renderList() }
       </View>
     );
   }
